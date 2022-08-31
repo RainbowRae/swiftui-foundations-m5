0a500d29f069ed8b52d027a37d0501935897b09e
Ian Bateman <ian.bateman@icloud.com>
2022-08-31 14:23:50 +0100

    Initial Commit

---
+++ MyLearning App/MyLearning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLearning_AppApp.swift
+//  MyLearning App
+//
+//  Created by Ian Bateman on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyLearning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
